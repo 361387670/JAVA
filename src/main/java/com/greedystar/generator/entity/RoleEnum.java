51ad79fda8ff6ce89cad061e95a7599a1994e120
sunyanwen <361387670@qq.com>
2021-08-18 12:22:46 +0800

    新增枚举

---
+++ src/main/java/com/greedystar/generator/entity/RoleEnum.java
@@ -0,0 +1,35 @@
+package com.greedystar.generator.entity;
+
+import com.mysql.cj.util.StringUtils;
+
+public class RoleEnum {
+
+    public enum Rolg {
+
+        /**
+         * 类型
+         */
+        LEASE("1", "租赁");
+
+        private String code;
+        private String name;
+
+        Rolg(String code, String name) {
+            this.code = code;
+            this.name = name;
+        }
+
+        public static String getNameByCode(String name) {
+            if (StringUtils.isNullOrEmpty(name)) {
+                return null;
+            }
+            for (Rolg rolg : Rolg.values()) {
+                if (name.equals(rolg.name)) {
+                    return rolg.code;
+                }
+            }
+            return null;
+        }
+
+    }
+}
